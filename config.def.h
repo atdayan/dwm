e47eb9728a395837f418a6fff7e2c77b7ea4ab2a
atdayan <adayanmc@gmail.com>
2021-01-15 09:53:30 -0300

    Set tag rules and tag icons

--- config.def.h
+++ config.def.h
@@ -20,16 +20,21 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "",  "", };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class		instance	title		tags mask	isfloating	monitor */
+	{ "Zathura",		NULL,		NULL,		1 << 1,		0,		0 },
+	{ NULL,			"Navigator",	NULL,		1 << 2,		0,		0 },
+	{ NULL,			NULL,		"lf",		1 << 3,		0,		0 },
+	{ NULL,			NULL,		"mutt",		1 << 4,		0,		0 },
+	{ "TelegramDesktop",	NULL,		NULL,		1 << 5,		0,		0 },
+	{ "discord",		NULL,		NULL,		1 << 5,		0,		0 },
+	{ "whatsdesk",		NULL,		NULL,		1 << 5,		0,		0 },
 };
 
 /* layout(s) */
